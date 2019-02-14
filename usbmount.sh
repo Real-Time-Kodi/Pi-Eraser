e053091132928991a8eabeeb7bbb4f47437c2efb
computerfr33k <computerfreak@computerfr33k.com>
2019-02-13 19:10:42 -0600

    Prevent script from being killed after 30 secs.

--- usbmount.sh
+++ usbmount.sh
@@ -4,7 +4,6 @@ devname=$(basename $1)
 logname=/tmp/$devname.log
 if [ "${ACTION}" = "add" ]
 then
-    /usr/local/bin/nuke.sh $1 >$logname &
-    disown
+    echo /usr/local/bin/nuke.sh $1 >$logname | at now
 fi
 
